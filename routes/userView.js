1e85f6f12d0ba0d8b93bd5c9573efc43691fe28a
lengocsangsang <lesang88@gmail.com>
2025-04-06 02:19:40 +0700

    check supplier info

--- routes/userView.js
+++ routes/userView.js
@@ -10,4 +10,8 @@ router.route("/").get((req, res, next) => {
   res.sendFile(path.join(__dirname, "../public/html/index.html")); // Use path.join to resolve the path
 });
 
+router.route("/checkSupplierInfo").get((req, res, next) => {
+  res.sendFile(path.join(__dirname, "../public/html/checkSupplierInfo.html")); // Use path.join to resolve the path
+});
+
 module.exports = router;
